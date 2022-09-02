5826e4282d0a6dc1c40bc95868609ddecf903675
JonathanErnst <warbotj@gmail.com>
2022-09-02 11:18:42 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+for (let i = 1; i <= 10; i++) {
+    let nombre = prompt("ingrese su nombre");
+    let salida = "Turno : " + i + " Nombre : " + nombre;
+    alert("hola sus datos son \n"   + salida);
+
+ }
